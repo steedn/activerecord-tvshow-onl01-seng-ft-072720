3c7bc0f47efccad49e125394a17de62c65b2a5a2
Nathan Steed <steedn@outlook.com>
2020-09-08 07:34:22 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,4 +6,8 @@ class Show < ActiveRecord::Base
   def self.most_popular_show
     self.where("rating = ?", self.highest_rating).first
   end
+  def self.lowest_rating
+    show = Show.minimum(:rating)
+    show
+  end
 end
